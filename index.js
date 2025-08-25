5f7ee66f289edbf3c32af81e807d9b0338b95147
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2025-08-25 15:18:43 +0300

    add resume

--- index.js
+++ index.js
@@ -25,3 +25,17 @@ const observer = new IntersectionObserver(handleImg, options);
 images.forEach(image => {
     observer.observe(image);
 })
+
+let accBtns = document.querySelectorAll('.experience__item');
+
+accBtns.forEach(btn => {
+    btn.addEventListener('click', function () {
+        this.classList.toggle('active');
+        let panel = this.querySelector('.experience__description');
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+    });
+})
